084ff7eae9f5afb6350f88436dc6f6aa0ae64a36
cybertramp <paran_son@outlook.com>
2019-05-01 00:02:24 +0900

    script folder added

---
+++ 8_scripts/rabbitmq_reset.sh
@@ -0,0 +1,26 @@
+﻿# Written by paran_son@outlook.com
+# updated 190417
+#
+# Rabbitmq reset and test account settting script
+# Caution! RabbitMQ Queue will be deleted.
+#
+# administrator: admin
+# user: test1, test2
+
+echo "###### RESET YOUR RABBITMQ ######"
+
+# reset rabbitmq-server
+sudo rabbitmqctl stop_app
+sudo rabbitmqctl reset
+sudo rabbitmqctl start_app
+
+# setup rabbitmq-server
+sudo rabbitmqctl add_user admin yana6728
+sudo rabbitmqctl set_user_tags admin administrator
+sudo rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
+
+sudo rabbitmqctl add_user test1 1234
+sudo rabbitmqctl set_permissions -p / test1 ".*" ".*" ".*"
+
+sudo rabbitmqctl add_user test2 1234
+sudo rabbitmqctl set_permissions -p / test1 ".*" ".*" ".*"
